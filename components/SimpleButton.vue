9785b0d9394d16cc5c195bd96412a98baaa7ba77
IbrahimOzde <Ibrahim.Ozdemir@theaccessgroup.com>
2023-08-18 09:46:05 +0300

    Simple Button component developed

---
+++ components/SimpleButton.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {
+  SimpleButtonColor,
+  SimpleButtonSize,
+} from "~/enums/components/simple-button.enum";
+
+interface SimpleButtonProps {
+  color?: SimpleButtonColor;
+  size?: SimpleButtonSize;
+  outline?: boolean;
+}
+
+const props = withDefaults(defineProps<SimpleButtonProps>(), {
+  outline: false,
+  size: SimpleButtonSize.Normal,
+  color: SimpleButtonColor.Default,
+});
+
+const classCombine = ref();
+
+const emit = defineEmits<{
+  (event: "click"): void;
+}>();
+
+onMounted(() => {
+  const outline = props.outline ? "btn-outline" : "";
+  classCombine.value = `btn ${props.size} ${props.color} ${outline}`;
+});
+</script>
+
+<template>
+  <button :class="classCombine" @click="emit('click')">
+    <slot></slot>
+  </button>
+</template>
---
+++ enums/components/simple-button.enum.ts
@@ -0,0 +1,13 @@
+export enum SimpleButtonSize {
+    Large = 'btn-lg',
+    Normal = '',
+    Small = 'btn-sm',
+    Tiny = 'btn-xs'
+}
+
+export enum SimpleButtonColor {
+    Default = '',
+    Primary = 'btn-primary',
+    Secondary = 'btn-secondary',
+    Accent = 'btn-accent'
+}
